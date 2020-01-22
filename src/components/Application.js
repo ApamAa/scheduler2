d665a8a88ddadad4b7bfc65aedb850af18a60b3a
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-01-22 15:04:23 -0800

    the hardcod from application.js(days array) removed and useEffect used

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "../components/DayList";
 import Appointment from "../components/Appointment";
 import "components/Application.scss";
-const appointments = [
+import axios from "axios";
+
+const hardCodedAppointments = [
   {
     id: 1,
     time: "12pm",
@@ -58,25 +60,17 @@ const appointments = [
     }
   }
 ];
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  const [appointments, setAppointments] = useState(hardCodedAppointments);
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then(response => {
+    console.log("response:", response);
+      setDays(response.data);
+    });
+  }, []);
   return (
     <main className="layout">
       <section className="sidebar">
