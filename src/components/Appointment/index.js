1b8b3d08fa455c262ae03e81be3ef2a728f00cec
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-01-23 20:40:50 -0800

    update the index.js and add save and book interview functions

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,33 +2,54 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Status from "components/Appointment/Status";
 import "components/Appointment/styles.scss";
 import useVisualMode from "hooks/useVisualMode"
 import Form from "components/Appointment/Form";
+// import { getInterviewersForDay } from "../../helpers/selectors";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const SAVING = "SAVING";
  
-export default function Appointment(props) {
-  const EMPTY = "EMPTY";
-  const SHOW = "SHOW";
-  const CREATE = "CREATE"
+export default function Appointment(props) {  
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    return interview;
+    // bookInterview(id, interview)
+    //   .then(() => transition(SHOW))
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-  {mode === SHOW && (
+    {mode === SHOW && (
     <Show
       student={props.interview.student}
       interviewer={props.interview.interviewer}
     />
     )}
     {mode === CREATE && ( <Form
-    interviewers = {[]}
+    interviewers = {props.interviewersDay}
+    
+    onSave={(name, interviewer) => {
+      transition(SAVING);
+      props.bookInterview(props.id,save(name, interviewer))
+      .then(() => transition(SHOW))
+    }}
     onCancel={() => back()}
     />)}
+    {mode === SAVING && <Status message="Saving" />}
     </article>
   )
 }
