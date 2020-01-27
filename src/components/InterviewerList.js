03291157a85a5863f3d0b511c10079d09e862a35
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-01-27 15:51:59 -0800

    added  Prop Types to the interviewerlist.js

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,13 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem"
 import "components/InterviewerList.scss";
+import PropTypes from "prop-types";
+
+InterviewerList.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func.isRequired
+};
+
 export default function InterviewerList(props) {
   const { interviewers, onChange, value } = props;
   const interviewerList = interviewers.map(({
@@ -25,3 +32,7 @@ export default function InterviewerList(props) {
     </section>
   );
 };
+
+
+
+
