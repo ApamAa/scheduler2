dfbfed30d25d70b83ee8985166d8248d76b538e3
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-01-28 21:02:40 -0800

    excluded index.js from test in package.json, made a test for reducer and refactoed reducer into src/reducer/application.js to achieve maximum test coverage

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,72 +2,17 @@ import React, { useEffect, useReducer } from "react";
 
 import {
   getInterviewersForDay,
-  findDayByAppointment
 } from "../helpers/selectors";
 
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+  UPDATE_SPOTS
+} from "reducers/application";
 import axios from "axios";
 // ------------------
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-const SET_INTERVIEWERS = "SET_INTERVIEWERS";
-const UPDATE_SPOTS = "UPDATE_SPOTS";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return { ...state, day: action.value };
-
-    case SET_APPLICATION_DATA:
-      return {
-        ...state,
-        days: action.days,
-        appointments: action.appointments,
-        interviewers: action.interviewers,
-        interviewersDay: action.interviewersDay
-      };
-
-    case SET_INTERVIEW:
-      const updated = {
-        ...state.appointments[action.id],
-        interview: action.interview
-      };
-      const updateAppointments = {
-        ...state.appointments,
-        [action.id]: updated
-      };
-      return { ...state, appointments: updateAppointments };
-    case SET_INTERVIEWERS:
-      const result = getInterviewersForDay(state, state.day);
-      return { ...state, interviewersDay: result };
-    case UPDATE_SPOTS:
-      const dayId = findDayByAppointment(action.id, state);
-      const aptIds = state.days[dayId].appointments;
-      let openSpots = 0;
-      for (let i = 0; i < aptIds.length; i++) {
-        if (!state.appointments[aptIds[i]].interview) {
-          openSpots += 1;
-        }
-      }
-      return {
-        ...state,
-        days: state.days.map((item, index) => {
-          if (index !== dayId) {
-            return item;
-          } else {
-            return {
-              ...item,
-              spots: openSpots
-            };
-          }
-        })
-      };
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
 // -------------------
 export default function useApplicationData() {
   const [state, dispatch] = useReducer(reducer, {
---
+++ src/reducers/application.js
@@ -0,0 +1,66 @@
+import {
+  // getInterviewersForDay,
+  findDayByAppointment
+} from "../helpers/selectors";
+
+export const SET_DAY = "SET_DAY";
+export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+export const SET_INTERVIEW = "SET_INTERVIEW";
+// export const SET_INTERVIEWERS = "SET_INTERVIEWERS";
+export const UPDATE_SPOTS = "UPDATE_SPOTS";
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case SET_DAY:
+      return { ...state, day: action.value };
+
+    case SET_APPLICATION_DATA:
+      return {
+        ...state,
+        days: action.days,
+        appointments: action.appointments,
+        interviewers: action.interviewers,
+        interviewersDay: action.interviewersDay
+      };
+
+    case SET_INTERVIEW:
+      const updated = {
+        ...state.appointments[action.id],
+        interview: action.interview
+      };
+      const updateAppointments = {
+        ...state.appointments,
+        [action.id]: updated
+      };
+      return { ...state, appointments: updateAppointments };
+    // case SET_INTERVIEWERS:
+    //   const result = getInterviewersForDay(state, state.day);
+    //   return { ...state, interviewersDay: result };
+    case UPDATE_SPOTS:
+      const dayId = findDayByAppointment(action.id, state);
+      const aptIds = state.days[dayId].appointments;
+      let openSpots = 0;
+      for (let i = 0; i < aptIds.length; i++) {
+        if (!state.appointments[aptIds[i]].interview) {
+          openSpots += 1;
+        }
+      }
+      return {
+        ...state,
+        days: state.days.map((item, index) => {
+          if (index !== dayId) {
+            return item;
+          } else {
+            return {
+              ...item,
+              spots: openSpots
+            };
+          }
+        })
+      };
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
